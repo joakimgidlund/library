03c1b494ef3f9011abb6f53e8f9c7e96262c8a16
jyoyu <jockegidlund@gmail.com>
2024-11-28 22:11:53 +0100

    Cleaned.

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.yrgo.library.models"},{"l":"se.yrgo.library.ui"},{"l":"se.yrgo.library.utilities"}];updateSearchResults();
